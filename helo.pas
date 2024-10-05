59e21bb3b8dcda61e4670054316069b9be715c3f
Нейжмак Анастасия <anastasiavolk95@gmail.com>
2024-10-05 08:18:50 +0300

    change hello logic

--- helo.pas
+++ helo.pas
@@ -1,4 +1,19 @@
-﻿program t1;
+﻿program task1;
 begin
-print('привет, мир');
+  var n:= readinteger;
+  assert(n>=0);
+  assert(n<=23);
+  case n of
+    4..10: print('Доброе утро,мир');
+    11..16:print('Добрый день,мир');
+    17..22: print('Добрый вечер,мир');
+    else print('Доброй ночи,мир');
+  end;
 end.
+{
+21
+Добрый вечер,мир 
+
+9
+Доброе утро,мир 
+}
