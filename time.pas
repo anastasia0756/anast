6532df45b33dea51323074a5a6ce93d5a51b3bbc
Нейжмак Анастасия <anastasiavolk95@gmail.com>
2024-10-05 08:21:33 +0300

    add new file

---
+++ time.pas
@@ -0,0 +1,15 @@
+﻿program task2;
+begin
+  var n:= readinteger;
+  if ((n mod 4) = 0) and ((n mod 100)<>0) and ((n mod 400)<>0) then
+    print('Високосный')
+  else 
+    print('Не високосный');
+end.
+{
+2020
+Високосный 
+
+2018
+Не високосный 
+}
